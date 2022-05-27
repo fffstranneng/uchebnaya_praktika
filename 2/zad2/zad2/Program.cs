4e894084a71876399114a28c0e32d2e644b96a55
fffstranneng <105425712+fffstranneng@users.noreply.github.com>
2022-05-27 09:08:11 +0300

    YEP

---
+++ 2/zad2/zad2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace z1
+{
+    internal class Program
+    {
+        class A
+        {
+            public int a;
+            public int b;
+
+            public double FirstCalculate(int a, int b)
+            {
+                return (a + b) / 2;
+            }
+            public double SecondCalculate(int a, int b)
+            {
+                return (Math.Pow(b, 3) + Math.Sqrt(a));
+            }
+            public A(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            A a = new A(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Средне арифметическое = {a.FirstCalculate(a.a, a.b)}");
+            Console.WriteLine($"Подсчёт функции: {a.SecondCalculate(a.a, a.b)}");
+        }
+    }
+}
---
+++ 2/zad2/zadd2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace z2
+{
+    class Organization
+    {
+        private string _name = "";
+        private string _product = "";
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Product
+        {
+            get { return _product; }
+            set { _product = value; }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"название организации:  {Name}, продукт организации:  {Product}");
+        }
+    }
+    class InsuranceCompany : Organization
+    {
+        public InsuranceCompany()
+        {
+            this.Name = "Страховая компания";
+            this.Product = "Страховка";
+        }
+    }
+    class OilAndGasCompany : Organization
+    {
+        public OilAndGasCompany()
+        {
+            this.Name = "Нефтегазовая компания";
+            this.Product = "Нефть и газ";
+        }
+    }
+    class Factory : Organization
+    {
+        public Factory()
+        {
+            this.Name = "завод";
+            this.Product = "салфетки";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsuranceCompany insurance = new InsuranceCompany();
+            insurance.Print();
+            OilAndGasCompany oilGas = new OilAndGasCompany();
+            oilGas.Print();
+            Factory factory = new Factory();
+            factory.Print();
+        }
+    }
+}
+
